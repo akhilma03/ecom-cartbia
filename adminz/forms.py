01245fa3b9e92c92896e6e6011bc1639ca5fa998
akhilma03 <akhilma4@gmail.com>
2022-07-02 23:02:37 +0530

    Changes product

--- adminz/forms.py
+++ adminz/forms.py
@@ -26,17 +26,17 @@ class ProductForm(forms.ModelForm):
         self.fields['category'].widget.attrs.update({'class':'form-control'})
         self.fields['sub_category'].widget.attrs.update({'class':'form-control'})
 
-        super().__init__(*args, **kwargs)
-        self.fields['sub_category'].queryset = Sub_Category.objects.none()
+        # super().__init__(*args, **kwargs)
+        # self.fields['sub_category'].queryset = Sub_Category.objects.none()
 
-        if 'category' in self.data:
-            try:
-                category_id = int(self.data.get('category'))
-                self.fields['sub_category'].queryset = Sub_Category.objects.filter(category_id=category_id).order_by('id')
-            except (ValueError, TypeError):
-                pass  # invalid input from the client; ignore and fallback to empty City queryset
-        elif self.instance.pk:
-            self.fields['sub_category'].queryset = self.instance.category.sub_category_set.order_by('id')
+        # if 'category' in self.data:
+        #     try:
+        #         category_id = int(self.data.get('category'))
+        #         self.fields['sub_category'].queryset = Sub_Category.objects.filter(category_id=category_id).order_by('id')
+        #     except (ValueError, TypeError):
+        #         pass  # invalid input from the client; ignore and fallback to empty City queryset
+        # elif self.instance.pk:
+        #     self.fields['sub_category'].queryset = self.instance.category.sub_category_set.order_by('id')
        
 
 
